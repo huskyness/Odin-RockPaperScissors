8791c97fa97e3a940c527364b8dac7b75670c91a
Eduardo Gomez <emvg.0996@gmail.com>
2022-08-07 10:29:49 -0500

    Add timer

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,16 +1,19 @@
 function playRound(playerSelection) {
+    document.getElementById("result").innerHTML="Calculating...";
     let computerSelection = Math.floor(Math.random() * 3)+1;
     let options = {
         1:"rock",
         2:"paper",
         3:"scissors"
     }
-    let textSelection = options[playerSelection]+' and computer choose was '+options[computerSelection];
-    if((playerSelection == 1 && computerSelection == 3) || (playerSelection == 2 && computerSelection == 1) || (playerSelection == 3 && computerSelection == 2)){
-        document.getElementById("result").innerHTML='You win! your choose was '+ textSelection;
-    }else if(playerSelection == computerSelection){
-        document.getElementById("result").innerHTML='Its a tie! your choose was '+ textSelection;
-    }else{
-        document.getElementById("result").innerHTML='You lose! your choose was ' + textSelection;
-    }
+    setTimeout(function(){
+        let textSelection = options[playerSelection]+' and computer choose was '+options[computerSelection];
+        if((playerSelection == 1 && computerSelection == 3) || (playerSelection == 2 && computerSelection == 1) || (playerSelection == 3 && computerSelection == 2)){
+            document.getElementById("result").innerHTML='You win! your choose was '+ textSelection;
+        }else if(playerSelection == computerSelection){
+            document.getElementById("result").innerHTML='Its a tie! your choose was '+ textSelection;
+        }else{
+            document.getElementById("result").innerHTML='You lose! your choose was ' + textSelection;
+        }
+    }, 1500); 
 }
