fbed6b519989eb1818cc8b61f802ae35c18fb8ed
Eduardo Gomez <emvg.0996@gmail.com>
2022-08-07 10:25:05 -0500

    Add message with results

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,10 +1,16 @@
 function playRound(playerSelection) {
     let computerSelection = Math.floor(Math.random() * 3)+1;
+    let options = {
+        1:"rock",
+        2:"paper",
+        3:"scissors"
+    }
+    let textSelection = options[playerSelection]+' and computer choose was '+options[computerSelection];
     if((playerSelection == 1 && computerSelection == 3) || (playerSelection == 2 && computerSelection == 1) || (playerSelection == 3 && computerSelection == 2)){
-        document.getElementById("result").innerHTML='You win!';
+        document.getElementById("result").innerHTML='You win! your choose was '+ textSelection;
     }else if(playerSelection == computerSelection){
-        document.getElementById("result").innerHTML='Its a tie!';
+        document.getElementById("result").innerHTML='Its a tie! your choose was '+ textSelection;
     }else{
-        document.getElementById("result").innerHTML='You lose!';
+        document.getElementById("result").innerHTML='You lose! your choose was ' + textSelection;
     }
 }
